ac09d9fda892f5544ad2700390973c9eb198e811
ivanov.io <iliya00817@gmail.com>
2023-05-30 13:23:47 +0300

    Added context version

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import org.example.configurations.AppConfigWithoutSpringBoot;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class App {
     public static void main(String[] args) {
+        var context = new AnnotationConfigApplicationContext(AppConfigWithoutSpringBoot.class);
+        AppConfigWithoutSpringBoot bean = context.getBean(AppConfigWithoutSpringBoot.class);
+        System.out.println(bean.getUrl());
 
     }
 }
--- src/main/java/org/example/configurations/AppConfigWithoutSpringBoot.java
+++ src/main/java/org/example/configurations/AppConfigWithoutSpringBoot.java
@@ -1,10 +1,12 @@
 package org.example.configurations;
 
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@Data
 @PropertySource("classpath:application.properties")
 public class AppConfigWithoutSpringBoot {
 
