b7e8fbdfc8d88045be12d196e27a6c9e256af8a6
ivanov.io <iliya00817@gmail.com>
2023-05-31 09:44:32 +0300

    Added context version

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,6 +4,7 @@ import org.example.configurations.AppConfigWithoutSpringBoot;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class App {
+
     public static void main(String[] args) {
         var context = new AnnotationConfigApplicationContext(AppConfigWithoutSpringBoot.class);
         AppConfigWithoutSpringBoot bean = context.getBean(AppConfigWithoutSpringBoot.class);
